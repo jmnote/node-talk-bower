33964ab9a3cf6e46236ae37d869db30a19f5b4d8
jmnote <banking@nate.com>
2016-10-11 11:21:55 +0900

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ io.on('connection', function(socket) {
 	console.log('a user connected');
 	socket.on('disconnect', function() {
 		console.log('user disconnected');
-	})
+	});
 	socket.on('message', function(msg){
 		io.emit('message', msg);
 	});
