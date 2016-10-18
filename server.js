bb2fbdbf0f096a9f794d7ce0b73b9673fabc1cea
jmnote <banking@nate.com>
2016-10-18 14:17:02 +0900

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.use('/', express.static('public'));
+app.use('/', express.static( __dirname + '/public'));
 
 io.on('connection', function(socket) {
 	console.log('a user connected');
